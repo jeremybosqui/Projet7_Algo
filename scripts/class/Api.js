90b40b76cb86dc5c6876c2f715048107d516e668
jeremybosqui <bosdani97@gmail.com>
2022-04-28 14:21:57 -0400

    instantiation Api.js + moove recipes.json files into scripts folder

---
+++ scripts/class/Api.js
@@ -0,0 +1,95 @@
+// instanciation de l'API 
+
+export default class Api {
+    // utilisation de la propriété static qui sont généralement des fonctions utilitaires qui peuvent permettre de créer ou de cloner des objets par exemple qui sont appelées sur la classe elle-même et pas sur le "prototype"
+    static recipes = []
+    static allIngredients = []
+    static allAppliances = []
+    static allUstensils = []
+
+    // connexion à la base de donnée via le fichier json contenant les données des recettes
+    static init = async () => {
+        const req = await fetch ('../recipes.json')
+        if(!req.ok) {
+            throw "Données momentanément indisponible"
+        }
+        const data = await req.json()
+        Api.recipes = data.recipes
+    }
+
+    /**
+     * recupere tous les ingrédents de toutes les recettes
+     *  @returns {array} instancier le tableau qui va contenir tous les ingrédents de toutes les recettes
+     */
+    static getAllIngredients = () => {
+        
+        if (Api.allIngredients.length === 0) {
+            Api.recipes.forEach(recipe => { // instancier une boucle forEach pour boucler sur chacun des ingredient
+                recipe.ingredient.map ( ingredients => { // utilisation de la methode map() qui crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant
+                    const ingredient = ingredients.ingredient
+                    
+                    if (!Api.allIngredients.includes(ingredient.toLowerCase())) {
+                        Api.allIngredients = [...Api.allIngredients, ingredient.toLowerCase()]
+                    }
+                })
+            })
+        }
+
+        return Api.allIngredients
+    }
+    /**
+     * recupere tous les appareils de toutes les recettes
+     *  @returns {array} instancier le tableau contenant tous les appareils de toutes les recettes
+     */
+    static getAllAppliances = () => {
+
+        if (Api.allAppliances.length === 0) {
+            Api.recipes.forEach ( recipe => {
+                
+            })
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+         
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
