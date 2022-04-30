dbc7594a0563e42e03a865dffda34226a8a54a01
jeremybosqui <bosdani97@gmail.com>
2022-04-30 14:16:52 -0400

    recipes.js update + add script eslint

--- scripts/class/Recipe.js
+++ scripts/class/Recipe.js
@@ -1,3 +1,4 @@
+import utils from "../modules/utils"
 
 
 
@@ -23,32 +24,59 @@ export default class Recipe {
      * @returns {HTMLElement}
      */
     view = () => {
+        const container = document.createElement('article')
+        container.setAttribute('class', 'recipes')
+
+        const image = document.createElement('img')
+        image.setAttribute('class','recipes__img')
+        image.setAttribute('src', 'http://via.placeholder.com/380x300')
+
+        const description = document.createElement('div')
+        description.setAttribute('class', 'recipes__description')
+
+        const descTop = document.createElement('div')
+        descTop.setAttribute('class', 'recipes__description__top')
+        descTop.innerHTML = `
+        <h2 class="name>${this.name}</h2>
+        <span class="duration><i class="far fa-clock"></i>${this.time} min</span>`
+
+        const ingredients = document.createElement('ul')
+        ingredients.setAttribute('class','ingredients-list')
         
+        this.ingredients.forEach(ingredient => {
+            ingredients.innerHTML += `<li class="ingredients-list__item"><span>${ingredient.ingredient}: </span>${ingredient.quantity} ${ingredient.unit || ''}</li>`
+        })
+
+        const process = document.createElement('p')
+        process.setAttribute('class', 'process')
+
+        if (this.description.length >= 200) {
+            process.innerText = utils.truncateStringEllipsis(this.description, 200)
+        } else {
+            process.innerHTML = this.description
+        }
+
+        const descBottom = document.createElement('div')
+        descBottom.setAttribute('class', 'recipes__description__bottom')
+
+        descBottom.appendChild(ingredients)
+        descBottom.appendChild(process)
+
+        description.appendChild(descTop)
+        description.appendChild(descBottom)
+
+        container.appendChild(image)
+        container.appendChild(description)
+
+        this.element = container
+
+        return container
+
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // basculer la visibilite entre visible et non visible
+    toggleVisibility = () => {
+        this.element.classList.toggle('hidden')
+        this.visible = !this.visible
+    }
 }
