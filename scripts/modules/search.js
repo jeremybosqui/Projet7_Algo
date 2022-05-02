1abc64c933d783e7bce2c788696539b3153b7d4a
jeremybosqui <bosdani97@gmail.com>
2022-05-02 12:39:48 -0400

    create search.js / first algo

---
+++ scripts/modules/search.js
@@ -0,0 +1,52 @@
+import FilterDropdown from "../class/FilterDropdown.js";
+import dom from "./dom.js";
+
+const search = (filters, recipes) => {
+    let principalSearch
+
+    if (document.getElementById('search-principal__input').value.length >= 3) {
+        principalSearch = document.getElementById('search-principal__input').value
+    }
+
+    recipes.forEach(recipe => {
+        const visible = true
+
+        if(filters !== []) {
+            const superArray = [recipe.appareils.toLowerCase()]
+            superArray = superArray.concat(recipe.ingredients.map(ingredients => ingredients.ingredient.toLowerCase()))
+            superArray = superArray.concat(recipe.ustensils.map(ustensil => ustensil.toLowerCase()))
+
+            filters.forEach(filter => {
+                if(!superArray.includes(filter.name.toLowerCase())) {
+                    visible = false
+                }
+            })
+
+        }
+
+        if(principalSearch !== undefined) {
+            const search = principalSearch
+            
+            recipe.ingredients.forEach(current => {
+                if (!current.ingredient.toLowerCase().includes(principalSearch) && !recipe.description.toLowerCase().includes(search) && !recipe.name.toLowerCase().includes(search)) {
+                    visible = false
+                }
+            })
+        }
+
+        if (recipe.element.classList.contains('hidden') === visible) {
+            recipe.toggleVisibility()
+        }
+    })
+
+    if (document.querySelectorAll('.recipes-container .recipes:not(.hidden)').length === 0) {
+        document.querySelector('.recipes-container .empty-msg').classList.add('visible')
+    } else {
+        document.querySelector('.recipes-container .empty-msg').classList.remove('visible')
+    }
+
+    FilterDropdown.updateDropdowns()
+
+}
+
+export default search
