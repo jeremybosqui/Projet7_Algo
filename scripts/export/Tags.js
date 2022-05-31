05ec6a052f86f717741921f7aef7d432509139b1
jeremybosqui <bosdani97@gmail.com>
2022-05-31 12:42:17 -0400

    summary

---
+++ scripts/export/Tags.js
@@ -0,0 +1,78 @@
+import DOM from "../modules/dom.js";
+import search from "../modules/search.js";
+import Recipe from "./Recipe.js";
+
+export default class Tags{
+  constructor(type, name) {
+    this.type = type;
+    this.name = name.toLowerCase();
+    
+    Tags.instances = [...Tags.instances, this];
+  }
+
+  static instances = [];
+  static active = []
+
+  /**
+     * Créer la vue d'un tag
+     * @returns {HTMLElement}
+     */
+  tag = () => {
+
+    if (this.tagRes) { return this.tagRes; }
+
+    let element = document.createElement('li');
+    element.setAttribute('class', `tag tag-${this.type}`);
+
+    let deleteBtn = document.createElement('i');
+    deleteBtn.setAttribute('class', 'far fa-times-circle tag__icon');
+    deleteBtn.addEventListener('click', this.delete)
+
+    element.innerText = this.name;
+    element.appendChild(deleteBtn);
+
+    this.tagRes = element;
+    return element;
+  }
+
+  /**
+     * Créer la vue d'un tag avec le style d'une list (pour dropdown)
+     * @returns {HTMLElement}
+     */
+  listElement = () => {
+
+    if (this.listElementRes) { return this.listElementRes; }
+
+    let element = document.createElement('li');
+    element.setAttribute('data-value', this.name);
+    element.innerText = this.name;
+
+    element.addEventListener('click', this.add);
+
+    this.listElementRes = element;
+    return element;
+  }
+
+  // ajout le tag au tags actif et l'affiche sur la page
+     
+  add = () => {
+    Tags.active = [...Tags.active, this];
+
+    DOM.append(this.tag(), document.getElementById('tags-list'));
+    search(Tags.active, Recipe.instances);
+    this.listElementRes.classList.add('already-selected');
+    
+  }
+
+  // supprime le tag des tags actif et le supprime de la page
+     
+  delete = () => {
+    let newActiveTags = Tags.active.filter(tag => tag !== this);
+    Tags.active = newActiveTags;
+
+    DOM.remove(this.tag());
+    search(Tags.active, Recipe.instances);
+    this.listElementRes.classList.remove('already-selected');
+    
+  }
+}
---
+++ scripts/utils/Recherche.js
@@ -0,0 +1,49 @@
+import FilterDropdown from '../class/Filtre_Dropdown.js';
+
+const search = (filters, recipes) => {
+  // instanciation des const
+  const searchBar = document.getElementById('search-principal__input');
+  const search = searchBar.value.length >= 3 ? searchBar.value : null; // operateur ternaire : si searchBar.value.length >= 3 alors searchBar = value sinon searchBar = null
+
+  // boucle sur chaque recette et test s'il y a une correspondance avec les filtres ou la recherche de l'utilisateur
+  recipes.forEach(recipe => {
+    let visible = true;
+    // initier les conditions
+    if (filters.length > 0) {
+      const appareils = [recipe.appareils.toLowerCase()];
+      const ingrédients = recipe.ingredients.map(ingredients => ingredients.ingredient.toLowerCase());
+      const ustensils = recipe.ustensils.map(ustensil => ustensil.toLowerCase());
+      const allFilters = [...appareils, ...ingrédients, ...ustensils];
+
+      filters.forEach(filter => {
+        if(!allFilters.includes(filter.name.toLowerCase())){
+          visible = false;
+        }
+      })
+    }
+    //
+    if (search) {
+      recipe.ingredients.forEach(current => {
+        if (!current.ingredient.toLowerCase().includes(search) && !recipe.description.toLowerCase().includes(search) && !recipe.name.toLowerCase().includes(search)) {
+          visible = false;
+        }
+      });
+    }
+    //
+    if (recipe.element.classList.contains("hidden") === visible) {
+      recipe.toggleVisibility();
+    }
+  });
+
+
+  FilterDropdown.updateDropDowns(); // met à jour les filtres disponibles
+
+  // si aucune recette ne correspond, affiche un message à l'utilisateur qu'aucune recette ne correspond à sa recherche
+  if (document.querySelectorAll('.recipes-container .recipes:not(.hidden)').length === 0) {
+    document.querySelector('.recipes-container .empty-msg').classList.add('visible');
+  }else{
+    document.querySelector('.recipes-container .empty-msg').classList.remove('visible');
+  }
+}
+
+export default search;
