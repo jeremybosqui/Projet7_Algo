f8c9f8edb916195cc5c70dc449ac5aecc0644023
jeremybosqui <bosdani97@gmail.com>
2022-06-08 12:33:12 -0400

    summary

---
+++ Scripts/Export/Class_Api.js
@@ -0,0 +1,101 @@
+//
+
+export default class api {
+ 
+  static recettes = []
+  static LsIngredients = []
+  static LsAppliances = []
+  static LsUstensils = []  
+
+  //
+
+  static requete = async function (loadRecipe) {
+    /*fetch('./recipes.json').then(function(response) {
+      if(response.ok) {
+        response.blob()
+      }
+    } )*/
+
+    /*const response = await fetch('./recipes.json')
+    const req = await response.json()
+    console.log(req)
+  }
+
+loadRecipe()*/
+    const request = await fetch ('./recipes.json')
+    const data = await request.json()
+    api.recettes = data.recipes
+    if (request.ok) {
+      console.log("Appel api réussi")
+    } else {
+      throw"Appel api échoué"
+    }
+    loadRecipe();
+  }
+  
+  /**
+   * @returns {array}
+   */
+  static setIngredients = () => {
+
+    if (api.LsIngredients.length === 0) {
+      api.recettes.forEach(recette => {
+        recette.ingredients.map(ingredients => {
+          const ingredient = ingredients.ingredient
+          if (api.LsIngredients.includes(ingredient)) {
+            api.LsIngredients = [...api.LsIngredients, ingredient]
+          }
+        })
+      })
+    }
+    return api.LsIngredients
+  }
+
+  /**
+ * @returns {array}
+ */
+  static setUstensils = () => {
+    if (api.LsUstensils.length === 0){
+      api.recettes.forEach(recette=> {
+        recette.ustensils.map(ustensil => {
+          if (!api.LsUstensils.includes(ustensil)){
+            api.LsUstensils = [...api.LsUstensils, ustensil]
+          }
+        })
+      })
+    }
+    return api.LsUstensils
+  }
+  /**
+   * @returns {array}
+   */
+  static setAppliances = () => {
+    if (api.LsAppliances.length === 0) {
+      api.recettes.forEach(recette => {
+        if (!api.LsAppliances.includes(recette.appliance)) {
+          api.LsAppliances = [...api.LsAppliances, recette]
+        }
+      })
+    }
+    return api.LsAppliances
+  }
+
+  static setEvryRecipes = () => {
+    return api.recettes
+  }
+
+  /**
+  * @param {number} // récupérer une recette par rapport à son id
+  * @returns {object} // init object dans lequel il y a les détails de recette
+  */
+  static setRecette = (id) => {
+    const recette = api.recettes.filter (recette => recette.id === id)
+    if (recette.length === 1) {
+      console.log("Aucun soucis de recette")
+    } else {
+      console.error("Recette indisponible")
+    }
+    return recette
+  }
+
+}
---
+++ Scripts/Tools/Scroll_Btn_Top.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const scrollToTopBtn = document.querySelector("#scrollToTopBtn");
+const rootElement = document.documentElement;
+
+function handleScroll() {
+  // donner les indications au bouton en lui disant de remonter en haut de la page et definir une hauteur
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal) > 0.45) {
+    // montrer le bouton
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // cacher le bouton grave a la suppression
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+function scrollToTop() {
+  // Scroll en haut de la page avec un comportement doux pour que le scroll ne soit pas violent vers le haut de la page
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
