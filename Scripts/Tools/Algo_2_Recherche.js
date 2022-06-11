da8af232c486f1e654f042e95e20a21eb2526394
jeremybosqui <bosdani97@gmail.com>
2022-06-11 18:48:41 -0400

    algo2 init

---
+++ Scripts/Tools/Algo_2_Recherche.js
@@ -0,0 +1,6 @@
+// mise en place des imports 
+//import DropdownFilters from '../Export/Class_Dropdown_Filter.js';
+//=============
+const algo2Recherche = () => {
+}
+export default algo2Recherche;
